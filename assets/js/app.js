db091b248159b6d1791c45873adbca6aa5224f3e
khushi <khushi.sce19@sot.pdpu.ac.in>
2023-06-07 18:16:06 +0530

    updated resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>  
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1Lx-8VlXLhd2TlmOQ9Jo1Xedtx4FfMXS8/view?usp=sharing" target="_blank">Resume</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1d3U6Edf9vgUl32F2wn97ir4ZGnlX6Dl1/view?usp=sharing" target="_blank">Resume</a></li>
    
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
